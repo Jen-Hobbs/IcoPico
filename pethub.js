d7ee278750ba8cd57076ab9ee54d3fd62d2e64d6
jennifer hobbs <jen.g.hobbs@gmail.com>
2019-05-02 15:38:35 -0700

    animation working

--- pethub.js
+++ pethub.js
@@ -5,26 +5,36 @@ class pethub extends Phaser.Scene{
     }
     init(data){
         console.log('init', data);
+        this.greetings = data.hi;
     }
      preload ()
     {
     this.load.image('backPet', 'testImages/sky.png');
     this.load.image("menuPet", 'testImages/menu.png');
+    this.load.image("button", 'testImages/button.png');
+    this.load.image('animate', 'testImages/animate.png');
     }
     
     create ()
     {
      this.add.sprite(this.scale.width/2, this.scale.height/2, 'backPet');    
+     
     let menu2 = this.add.sprite(this.scale.width*.05, this.scale.height*.05, 'menuPet');    
         menu2.setInteractive();
         menu2.on('pointerdown', ()=> {  
             this.scene.run('ShowMenu'); 
             this.scene.bringToTop('ShowMenu');
-        })
+        });
+        let button = this.add.sprite(this.scale.width*.5, this.scale.width*.5, 'button');
+        button.setInteractive();
+        button.on('pointerdown', ()=> {
+            console.log("hi");
+        });
     }
     update(){
 
-        // console.log(data.hi);
+            //  console.log(this.greetings);
+
     }
     }
     var config = {
@@ -43,6 +53,5 @@ class pethub extends Phaser.Scene{
     };
     
     var game = new Phaser.Game(config);
-    
 
     
