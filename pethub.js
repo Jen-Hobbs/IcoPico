64658e6d9fcb43adec271929246444aff41ea1b4
Jen-Hobbs <jen.g.hobbs@gmail.com>
2019-05-01 20:33:42 -0700

    reverting commits

--- pethub.js
+++ pethub.js
@@ -1,6 +1,7 @@
 
 class pethub extends Phaser.Scene{
 constructor(){
+<<<<<<< HEAD
     super({key:'Pethub', active: true})
 }
 
@@ -23,22 +24,55 @@ let menu2 = this.add.sprite(this.scale.width*.05, this.scale.height*.05, 'menu')
 }
 update(){
 
+=======
+    super({key:'pethub', active: true})
+}
+
+ preload ()
+{
+background = this.load.image('sky', 'testImages/sky.png');
+this.load.image('button', 'testImages/button.png')
+}
+
+create ()
+{
+
+
+background = this.add.sprite(this.scale.width/2, this.scale.height/2, 'sky');    
+
+button = this.add.sprite(this.scale.width/2, this.scale.height/2, 'button');
+button.setInteractive();
+button.on('pointerdown', () => {this.scene.start("shop")})
+
+>>>>>>> parent of cb1994f... Merge branch 'Joanna'
 }
 }
 var config = {
 
     parent: 'wrapper',
     scale: {
+<<<<<<< HEAD
         mode: Phaser.Structs.Size.WIDTH_CONTROLS_HEIGHT ,
+=======
+        mode: Phaser.Scale.FIT ,
+>>>>>>> parent of cb1994f... Merge branch 'Joanna'
         width: 800,
         height:400,
         type: Phaser.AUTO,
                
     },
+<<<<<<< HEAD
     scene: [pethub, Shop, Task, Bag, ShowMenu]
     
+=======
+    scene: [pethub, shop]
+>>>>>>> parent of cb1994f... Merge branch 'Joanna'
 };
 
 var game = new Phaser.Game(config);
 var background;
+<<<<<<< HEAD
 var button;
+=======
+var button;
+>>>>>>> parent of cb1994f... Merge branch 'Joanna'
--- shop.js
+++ shop.js
@@ -3,6 +3,7 @@ class Shop extends Phaser.Scene {
         super({key:"Shop", active:false});
     }
     preload(){
+<<<<<<< HEAD
         this.load.image("menu", 'testImages/menu.png')
     }
     create(){
@@ -14,6 +15,14 @@ class Shop extends Phaser.Scene {
         menu2.on('pointerdown', ()=> {
             this.scene.launch('ShowMenu');     
         })
+=======
+        this.load.image('sky', 'testImages/sky.png');
+    }
+    create(){
+        this.add.sprite(this.scale.width/2, this.scale.height/2, 'sky');    
+
+        this.add.text(20, 20, "text");
+>>>>>>> parent of cb1994f... Merge branch 'Joanna'
     }
     
 }
