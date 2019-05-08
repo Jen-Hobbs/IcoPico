128f5d8bfdc65139b0f5cf0c7e6fe2aecdedb328
jennifer hobbs <jen.g.hobbs@gmail.com>
2019-05-07 18:46:16 -0700

    button click

--- pethub.js
+++ pethub.js
@@ -1,19 +1,17 @@
 
-class pethub extends Phaser.Scene{
-    constructor(){
-        super({key:'Pethub', active: true})
-
-        
+class pethub extends Phaser.Scene {
+    constructor() {
+        super({ key: 'Pethub', active: true })
+        this.newInput = 0;
     }
-    init(data){
+    init(data) {
         // console.log('init', data);
         // this.greetings = data.hi;
         // this.check = 0;
-        
+
     }
-     preload ()
-    {
-        for(var i = 0; i < information.length; i++){
+    preload() {
+        for (var i = 0; i < information.length; i++) {
             this.load.image('pet' + i, 'images/pets/' + information[i] + '.png');
         }
         this.load.image('arrow', 'images/buttons/Other/arrow.png');
@@ -25,171 +23,183 @@ class pethub extends Phaser.Scene{
         this.load.image('whiteCircle', 'images/icons/whiteCircle.png');
         this.load.image('yellowCircle', 'images/icons/yellowCircle.png');
         this.load.image('foodButton', 'images/buttons/scene_button/food_Button.png');
-        
+
     }
-    
-    create ()
-    {
-        this.cameras.main.setBounds(0, 0, 1236*information.length , 681);
+
+    create() {
+        this.cameras.main.setBounds(0, 0, 1236 * information.length, 681);
         this.cameras.main.setBackgroundColor('#aaa');
-        var pet = [];
+        this.pet = [];
         var arrowR = [];
         var arrowL = [];
-        
+
         //create container for all information about pet and Flip between pets
-        for(var i = 0; i < information.length; i++){
+        for (var i = 0; i < information.length; i++) {
             var pos = 0;
-            
+
             //right arrow
-            arrowR[i] = this.add.sprite(this.scale.width*0.95, this.scale.height/2, 'arrow');
+            arrowR[i] = this.add.sprite(this.scale.width * 0.95, this.scale.height / 2, 'arrow');
             arrowR[i].setInteractive();
-            arrowR[i].on('pointerdown', ()=>{
+            arrowR[i].on('pointerdown', () => {
                 var cam = this.cameras.main;
-                if(pos < information.length - 1){
+                if (pos < information.length - 1) {
                     pos++;
                 }
-                else{
+                else {
                     pos = 0;
                 }
-                cam.centerOn(618 + 1236*pos, 0);
+                cam.centerOn(618 + 1236 * pos, 0);
             });
             //left arrow
-            arrowL[i] = this.add.sprite(this.scale.width*0.04, this.scale.height/2, 'arrow');
+            arrowL[i] = this.add.sprite(this.scale.width * 0.04, this.scale.height / 2, 'arrow');
             arrowL[i].flipX = !arrowL[i].flipX;
             arrowL[i].setInteractive();
-            arrowL[i].on('pointerdown', ()=>{
+            arrowL[i].on('pointerdown', () => {
                 var cam = this.cameras.main;
-                if(pos == 0){
+                if (pos == 0) {
                     pos = information.length - 1;
                 }
-                else{
+                else {
                     pos--;
                 }
-                cam.centerOn(618 + 1236*pos, 0);
+                cam.centerOn(618 + 1236 * pos, 0);
             });
             //menu button
-            let menu = this.add.sprite(this.scale.width*.04, this.scale.height*.05, 'menuPet');    
+            let menu = this.add.sprite(this.scale.width * .04, this.scale.height * .05, 'menuPet');
             menu.setInteractive();
-            menu.on('pointerdown', ()=> {  
-                this.scene.run('ShowMenu'); 
+            menu.on('pointerdown', () => {
+                this.scene.run('ShowMenu');
                 this.scene.bringToTop('ShowMenu');
             });
 
 
 
-            pet[i] = this.add.container(i*1236, 0);
-            pet[i].add(this.add.sprite(this.scale.width/2, this.scale.height/2, 'backPet')); //background
-            pet[i].add(this.add.sprite(this.scale.width/2, this.scale.height/2, 'pet' + i)); //addpet
-            pet[i].add(arrowR[i]);
-            pet[i].add(arrowL[i]);
-            pet[i].add(menu);
-            this.checkHappiness(i, pet);
+            this.pet[i] = this.add.container(i * 1236, 0);
+            this.pet[i].add(this.add.sprite(this.scale.width / 2, this.scale.height / 2, 'backPet')); //background
+            this.pet[i].add(this.add.sprite(this.scale.width / 2, this.scale.height / 2, 'pet' + i)); //addpet
+            this.pet[i].add(arrowR[i]);
+            this.pet[i].add(arrowL[i]);
+            this.pet[i].add(menu);
+            this.checkHappiness(i, this.pet);
+
+        }
+        this.foodUpdate();
+    }
+    update() {
+        if(this.newInput == 1){
+            this.newInput = 0;
+            for(var i = 0; i < 3; i++){
+                for(var j = 0; j < 3; j++){
+                    // this.pet[i].
+                }
+            }
+        }
+
+    }
+    foodUpdate() {
+        for (var i = 0; i < this.pet.length; i++) {
+            this.displayfood(this.pet[i]);
             
         }
-        this.foodUpdate(pet);
+        this.input.on('gameobjectup', function (pointer, gameObject) {
+            gameObject.emit('clicked', gameObject);
+             console.log('hi');
+        }, this);
     }
-    update(){
 
 
-    }
-    foodUpdate(pet){
-        for(var i = 0; i < pet.length; i++){
-            this.food(pet[i], pet);
-        }
-    }
-   
+    displayfood(pet) {
 
-    food(pet, info){
-        
-        
+
 
         var food = [];
         let showfood = 0;
-        for(var i = 0; i < player.food.length; i++){
-            food[i] = this.add.sprite(this.scale.width*(.85 - (i*.10)), this.scale.height*.95, 'whiteCircle');
+        for (var i = 0; i < player.food.length; i++) {
+            food[i] = this.add.sprite(this.scale.width * (.85 - (i * .10)), this.scale.height * .95, 'whiteCircle');
             food[i].setInteractive();
-            
+            food[i].name = i;
 
             food[i].on('clicked', this.location, this);
 
-            console.log('hi');
-       
+
         }
-        this.input.on('gameobjectup', function (pointer, gameObject)
-    {
-        gameObject.emit('clicked', gameObject);
-    }, this);
-        let yellow = this.add.sprite(this.scale.width*.95, this.scale.height*.95, 'yellowCircle');
-        let white = this.add.sprite(this.scale.width*.95, this.scale.height*.95, 'whiteCircle');
+       
+
+
+
+
+
+        let yellow = this.add.sprite(this.scale.width * .95, this.scale.height * .95, 'yellowCircle');
+        let white = this.add.sprite(this.scale.width * .95, this.scale.height * .95, 'whiteCircle');
         pet.add(yellow);
         pet.add(white);
         pet.add(food);
-        for(var i = 0; i < player.food.length; i++){
+        for (var i = 0; i < player.food.length; i++) {
             pet.sendToBack(food[i]);
             food[i].disableInteractive();
         }
         white.setInteractive();
-        
+
         white.on('pointerdown', () => {
-            if(showfood == 0){
+            if (showfood == 0) {
                 pet.bringToTop(yellow);
-                for(var i = 0; i < player.food.length; i++){
+                for (var i = 0; i < player.food.length; i++) {
                     pet.bringToTop(food[i]);
                     food[i].setInteractive();
                 }
-                 showfood = 1;
+                showfood = 1;
             }
-            else{
+            else {
                 showfood = 0;
-                for(var i = 0; i < player.food.length; i++){
+                for (var i = 0; i < player.food.length; i++) {
                     pet.sendToBack(food[i]);
                     food[i].disableInteractive();
                 }
                 pet.sendToBack(yellow);
             }
         })
-        
-        
+        // pet.add(food);
+
     }
 
-    location(box){
-        // console.log('hi');
+    location(box) {
+        console.log(box.name);
         // console.log(food);
         box.off('clicked', location);
         box.input.enabled = false;
         box.setVisible(false);
+        
     }
-    
-    checkHappiness(i, pet){
-        if(player.happiness[i] < 33){
+
+    checkHappiness(i, pet) {
+        if (player.happiness[i] < 33) {
             pet[i].add(this.add.sprite(this.scale.width * .95, this.scale.height * .07, 'blackHeart'));
         }
-        else if(player.happiness[i] < 66){
+        else if (player.happiness[i] < 66) {
             pet[i].add(this.add.sprite(this.scale.width * .95, this.scale.height * .07, 'yellowHeart'));
         }
-        else{
+        else {
             pet[i].add(this.add.sprite(this.scale.width * .95, this.scale.height * .07, 'redHeart'));
         }
     }
-    }
-    
-    let petNumber = 3;
-    var config = {
-    
-        parent: 'wrapper',
-        scale: {
-            mode: Phaser.Scale.FIT ,
-            width: 1236,
-            height: 681,
-            type: Phaser.AUTO,
-            autoCenter: Phaser.Scale.autoCenter
-                   
-        },
-        scene: [ShowMenu, Shop, Task, Bag, pethub]
-        
-    };
-    
-    var game = new Phaser.Game(config);
-    var player;
-    var display;
-    
+}
+
+let petNumber = 3;
+var config = {
+
+    parent: 'wrapper',
+    scale: {
+        mode: Phaser.Scale.FIT,
+        width: 1236,
+        height: 681,
+        type: Phaser.AUTO,
+        autoCenter: Phaser.Scale.autoCenter
+
+    },
+    scene: [ShowMenu, Shop, Task, Bag, pethub]
+
+};
+
+var game = new Phaser.Game(config);
+var player;
+var display;
