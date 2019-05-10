12396c7f2dc9ff78ec4c182276abe3e7c018aff1
nj-keng <nicole.jingco@gmail.com>
2019-05-10 13:40:36 -0700

    updated the Nic.Feature2.1 branch with DevBranch files

--- src/scripts/setup-firebase.js
+++ src/scripts/setup-firebase.js
@@ -1,17 +1,12 @@
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
+var config = {
     apiKey: "AIzaSyBa4tgLd5vJicvFJ_jW7gR2DaoiX6kN9-A",
     authDomain: "icopico-89023.firebaseapp.com",
     databaseURL: "https://icopico-89023.firebaseio.com",
     projectId: "icopico-89023",
     storageBucket: "icopico-89023.appspot.com",
-    messagingSenderId: "915607181568",
-    appId: "1:915607181568:web:5015da4ac78a8615"
+    messagingSenderId: "117462095616"
   };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-
-  function sign_out_function() {
-    location.href = "../index.html";
-    firebase.auth().signOut();
-  };
+  firebase.initializeApp(config);
+  
+  var database = firebase.database();
+  
