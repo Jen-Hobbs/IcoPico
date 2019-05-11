3839a33d2f6cee90c39e5c8e3dfdd011800df42c
joannakwh <joannakwh@gmail.com>
2019-05-10 19:43:48 -0700

    Tasks displaying title, description, get new, and icon

--- src/scripts/Task.js
+++ src/scripts/Task.js
@@ -13,12 +13,18 @@ class Task extends Phaser.Scene {
         this.load.image("type2", '../images/buttons/pet_hub/red_heart.png');
         this.load.image("type3", '../images/buttons/pet_hub/yellow_heart.png');
         this.load.image("menuPet", '../images/buttons/Other/menu.png');
+        this.load.image("get_new", '../images/buttons/task_hub/get_new.png');
         this.cameras.main.setBackgroundColor('#aaa');  
     }
 
     create() {
         var MAX_NUM_TASKS = 3;
         var taskButtons = [];
+
+        //Title
+        var shop = this.add.text(this.scale.width/2, this.scale.height*.10, "", {fontFamily: 'serif', fontSize: 64});
+        shop.setOrigin(0.5);
+        shop.setColor('black');
 
         //Placeholder Text
         this.add.text(this.scale.width/2, this.scale.height/2, "task"); 
@@ -32,7 +38,6 @@ class Task extends Phaser.Scene {
         });
 
         //this.add.sprite(this, 100, 100, task_new);    
-
         //console.log(Tasks_example.tasks[1].type);
 
         for(let i = 0; i < MAX_NUM_TASKS; i++)
@@ -40,9 +45,20 @@ class Task extends Phaser.Scene {
             taskButtons[i] = this.add.container(this.scale.width/2, 150*i + 200);
             var sprite = this.add.sprite(0,0,'task_new').setScale(0.3);
             var index = i;
-            var icon = type1;
-            var 
-            taskButtons[i].add([sprite]);
+            var icon = this.add.sprite(125,5,'type2').setScale(0.7);
+            var new_task = this.add.sprite(175,-40,'get_new')
+                .setScale(1.3);
+                // .setInteractive({ useHandCursor: true })
+                // .on('pointerup', () => {}
+                    
+            var title = this.add.text(-175,-45,"Test title " + i,
+                {fontFamily: 'serif', fontSize: 25, wordWrap: true, wordWrapWidth: 500})
+                .setColor('black');
+
+            var description = this.add.text(-175, -15, "This is a really cool description. Why is it not wrapping sdljf dskfjdsf dkjfhsdkf skdjf hsdkf jshdfadsfl dsfjdf kjdshf",
+                {fontFamily: 'serif', fontSize: 20, wordWrap: true, wordWrapWidth: 100})
+                .setColor('black');
+            taskButtons[i].add([sprite, icon, new_task, title, description]);
 
           //instantiate new button
           //constructor(scene, x, y, texture, index, icon, type, title, description, isActive, callback)
