70d254e859f9a3e79614690ad3834e5ad87e5e1b
joannakwh <joannakwh@gmail.com>
2019-05-13 11:38:17 -0700

    save

--- src/scripts/Task.js
+++ src/scripts/Task.js
@@ -21,13 +21,14 @@ class Task extends Phaser.Scene {
         var MAX_NUM_TASKS = 3;
         var taskButtons = [];
 
-        //Title
-        var shop = this.add.text(this.scale.width/2, this.scale.height*.10, "", {fontFamily: 'serif', fontSize: 64});
+        var shop = this.add.text(this.scale.width/2, this.scale.height*.10, "", {fontFamily: 'Helvetica', fontSize: 64});
         shop.setOrigin(0.5);
         shop.setColor('black');
 
-        //Placeholder Text
-        this.add.text(this.scale.width/2, this.scale.height/2, "task");
+        //Text to show up when there are no more tasks
+        this.add.text(this.scale.width/2, 200, "All tasks complete. Great job!", {fontFamily: 'Helvetica', fontSize: 25})
+          .setOrigin(0.5)
+        ;
 
         //Create Menu Button
         let menu = this.add.sprite(this.scale.width*.04, this.scale.height*.05, 'menuPet');
@@ -44,11 +45,13 @@ class Task extends Phaser.Scene {
         var dismiss = this.add.sprite(this.scale.width/2, this.scale.height-100, "type1")
           .setScale(0.3)
           .setInteractive({ useHandCursor: true})
+          .setVisible(false)
           .on('pointerup', () => {
-
+            Console.log("dismiss");
           });
 
           this.scene.sendToBack('type2');
+
         for(let i = 0; i < MAX_NUM_TASKS; i++)
         {
             taskButtons[i] = this.add.container(this.scale.width/2, 150*i + 200);
@@ -63,7 +66,7 @@ class Task extends Phaser.Scene {
                 .setData('rindex', i)
                 .setScale(1.3)
                 .setInteractive({ useHandCursor: true })
-                .on('clicked', getNewTask, this);
+                .on('clicked', deleteTask, this);
 
             var title = this.add.text(-175,-45,"Test title " + i,
                 {fontFamily: 'Helvetica', fontSize: 20, wordWrap: {width: 265, useAdvancedWrap:true}})
@@ -82,21 +85,7 @@ class Task extends Phaser.Scene {
             {
                 gameObject.emit('clicked', gameObject);
             }, this);
-
-
-          //instantiate new button
-          //constructor(scene, x, y, texture, index, icon, type, title, description, isActive, callback)
-        //   taskButtons[i] = new TaskButton
-        //     (this, this.scale.width/2, 150*i + 200,
-        //         ['task_new', i, 3, "Test title" + i, "This is a really cool description", false, () => this.setSelected()])
-
-        //     //     'task_new', i, 3,
-        //     //   "Test Title" + i, "This is a really cool description", false, () => this.setSelected());
-
-        //   //add button to canvas
-        //   this.add.existing(taskButtons[i])
-        //   .setScale(0.3);
-        }
+        }//end for 
 
         //callback method for click
         function setSelected (tSprite) {
@@ -109,9 +98,23 @@ class Task extends Phaser.Scene {
           taskButtons[i].replace(sprite,newSprite,[,true]);
         }
 
-        function getNewTask (rSprite) {
+        function deleteTask (rSprite) {
           var i = rSprite.getData('rindex');
           taskButtons[i].destroy();
+        }
+        
+        function floatUp(deletedIndex)
+        {
+          for(deletedIndex; i < MAX_NUM_TASKS; deletedIndex++)
+          {
+            taskButtons[deletedIndex] = taskButtons[deletedIndex-1];
+          }
+        }
+
+        function refreshTasklist()
+        {
+          var temp = [];
+          temp = taskButtons;
         }
     }//end create
 
