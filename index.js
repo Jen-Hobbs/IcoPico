fb0fd7fc2c2d14f0675de384faf5456c47a24eec
Mikhaela Layon <mapl.layon@gmail.com>
2019-05-23 20:11:03 -0700

    changing port

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const express = require('express');
 const mysql = require('mysql');
 const app = express();
 const path = require('path');
-const port= process.env.PORT || 8080;
+//const port= process.env.PORT || 8080;
 
 // Create connection
 const db = mysql.createConnection({
@@ -502,7 +502,7 @@ app.get('/checkaccount/:email', (req, res) => {
 
 });
 
-var server = app.listen(port, function(){
-    //var port = server.address().port;
+var server = app.listen(8080, function(){
+    var port = server.address().port;
     console.log("Server started at http://localhost:%s", port);
 });
