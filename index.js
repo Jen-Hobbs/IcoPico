0f48c74613f188a3e46b40dd5aafb58a3aaed1cc
Mikhaela Layon <mapl.layon@gmail.com>
2019-10-07 18:37:27 -0700

    debugging hosting issue

--- index.js
+++ index.js
@@ -26,9 +26,9 @@ db.connect(function (err) {
 app.use(express.static('./'));
 app.use(express.static('src'));
 app.use('index.html', express.static('index.html'));
-app.use('/css', express.static(__dirname + '/src/css'));
-app.use('/scripts', express.static(__dirname + '/src/scripts'));
-app.use('/images', express.static(__dirname + '/src/images'));
+app.use('*/css', express.static(__dirname + '/src/css'));
+app.use('*/scripts', express.static(__dirname + '/src/scripts'));
+app.use('*/images', express.static(__dirname + '/src/images'));
 app.use('login.html', express.static('/src/login/login.html'));
 app.use('icopico.html', express.static('/src/icopico/icopico.html'));
 app.use('icopico.html', express.static('/src/icopicoPet/icopico.html'));
@@ -468,7 +468,7 @@ app.get('/checkaccount/:email', (req, res) => {
 
 });
 
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 8000;
 var server = app.listen(port, function(){
    // var port = server.address().port;
     console.log("Server started at http://localhost:%s", port);
