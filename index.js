c67b2edbf22deede2650498be71e63c800a2380e
Mikhaela Layon <mapl.layon@gmail.com>
2019-10-07 18:51:37 -0700

    debugging hosting issue

--- index.js
+++ index.js
@@ -26,9 +26,9 @@ db.connect(function (err) {
 app.use(express.static('./'));
 app.use(express.static('src'));
 app.use('index.html', express.static('index.html'));
-app.use('*/css', express.static(__dirname + '/src/css'));
-app.use('*/scripts', express.static(__dirname + '/src/scripts'));
-app.use('*/images', express.static(__dirname + '/src/images'));
+app.use('/css', express.static(__dirname + '/src/css'));
+app.use('/scripts', express.static(__dirname + '/src/scripts'));
+app.use('/images', express.static(__dirname + '/src/images'));
 app.use('login.html', express.static('/src/login/login.html'));
 app.use('icopico.html', express.static('/src/icopico/icopico.html'));
 app.use('icopico.html', express.static('/src/icopicoPet/icopico.html'));
